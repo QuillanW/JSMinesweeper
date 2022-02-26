7f3ddbff8f662ae1d5e512b7c96e65870ec2c9de
QuillanW <quillanwielhouwer@live.nl>
2022-02-26 22:49:20 +0100

    update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function create() {
     let l = 0;
 
     for (let i = 0; i < bombs; i++) {
-        newBombLocation = Math.round(Math.random()*(sizeX * sizeY))
+        newBombLocation = Math.round(Math.random() * (sizeX * sizeY))
         for (let l = 0; l < bombLocations.length; l++) {
             if (newBombLocation == bombLocations[l]) {
                 newBombLocation++;
@@ -34,10 +34,10 @@ function create() {
 
     for (let i = 0; i < (sizeX * sizeY); i++) {
         var newButton = document.createElement('button');
-        newButton.id = 'spot' + i;
+        newButton.id = i;
         newButton.classList.add('spotBtn');
         newButton.addEventListener('click', checkHit);
-        
+
         if (posX == sizeX) {
             posX = 0;
             posY++;
@@ -45,22 +45,39 @@ function create() {
             posX++;
         }
         newButton.style.grid = posX + '/' + posY;
-        newButton.name = (posX) + "" + posY
         gameContainer.appendChild(newButton);
     }
 }
 
 function checkHit() {
-    clicked = event.target.id.replace("spot", "");
+    console.log('checking')
+    clicked = event.target.id;
     clickedBtn = event.target
-
+    clickedXY = clicked / sizeX + 1.1
     let bombsNear = 0;
-
-    checkPos = clicked - 1
-
+    let direction = 0;
+    let fraction = 1 / sizeX;
+    
     for (let i = 0; i < bombLocations.length; i++) {
-        if (checkPos == bombLocations[i]) {
-            bombsNear++;
+        bombXY = (bombLocations[i] / sizeX + 1.1).toFixed(4)
+        for (let l = 0; l < 8; l++) {
+            if (l == 0) {direction = -1 - fraction;} 
+            else if (l == 1) {direction = -1;}
+            else if (l == 2) {direction = -1 + fraction;}
+            else if (l == 3) {direction = -fraction;}
+            else if (l == 4) {direction = fraction;}
+            else if (l == 5) {direction = 1 - fraction;}
+            else if (l == 6) {direction = 1;}
+            else if (l == 7) {direction = 1 + fraction;}
+            
+            checkPos = (parseFloat(clickedXY) + direction).toFixed(4)
+
+            console.log(checkPos, bombXY)
+
+            if (checkPos == bombXY) {
+                bombsNear++;
+                console.log('found')
+            }
         }
     }
 
@@ -70,6 +87,9 @@ function checkHit() {
             clickedBtn.classList.add('boom')
         } else if (clicked != bombLocations[i]) {
             clickedBtn.classList.add('safe')
+            if (bombsNear == 0) {
+                bombsNear = ""
+            }
             clickedBtn.innerHTML = bombsNear;
         }
     }
