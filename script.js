beca97c8fc1d60e7854934b5a7e9a0636c918728
QuillanW <quillanwielhouwer@live.nl>
2022-02-27 11:41:08 +0100

    update

--- script.js
+++ script.js
@@ -8,16 +8,15 @@ function create() {
     sizeY = document.getElementById('sizeY').value;
     bombs = document.getElementById('bombInput').value;
 
-    let l = 0;
-
     for (let i = 0; i < bombs; i++) {
         newBombLocation = Math.round(Math.random() * (sizeX * sizeY))
         for (let l = 0; l < bombLocations.length; l++) {
             if (newBombLocation == bombLocations[l]) {
-                newBombLocation++;
+                newBombLocation = -100;
+                i--;
             }
         }
-        bombLocations.push(newBombLocation)
+        bombLocations.push(newBombLocation);
     }
 
     console.log(bombLocations)
@@ -36,7 +35,8 @@ function create() {
         var newButton = document.createElement('button');
         newButton.id = i;
         newButton.classList.add('spotBtn');
-        newButton.addEventListener('click', checkHit);
+        newButton.addEventListener('mouseup', checkHit);
+        newButton.addEventListener('mouseup', checkHit);
 
         if (posX == sizeX) {
             posX = 0;
@@ -57,26 +57,41 @@ function checkHit() {
     let bombsNear = 0;
     let direction = 0;
     let fraction = 1 / sizeX;
-    
+    onBegin = (clickedXY - 0.1 - Math.floor(clickedXY)).toFixed(1) == 0
+    onEnd = (clickedXY - 1.1 - Math.floor(clickedXY - 1.1)).toFixed(4) == (1 - fraction).toFixed(4);
+
     for (let i = 0; i < bombLocations.length; i++) {
         bombXY = (bombLocations[i] / sizeX + 1.1).toFixed(4)
-        for (let l = 0; l < 8; l++) {
-            if (l == 0) {direction = -1 - fraction;} 
-            else if (l == 1) {direction = -1;}
-            else if (l == 2) {direction = -1 + fraction;}
-            else if (l == 3) {direction = -fraction;}
-            else if (l == 4) {direction = fraction;}
-            else if (l == 5) {direction = 1 - fraction;}
-            else if (l == 6) {direction = 1;}
-            else if (l == 7) {direction = 1 + fraction;}
-            
-            checkPos = (parseFloat(clickedXY) + direction).toFixed(4)
 
-            console.log(checkPos, bombXY)
+        if (onBegin) {
+            checkFront = 1;
+            checkBack = 10000000;
+        } else if (onEnd) {
+            checkFront = 10000000;
+            checkBack = 1;
+        } else {
+            checkFront = 1;
+            checkBack = 1;
+        }
+        
+        for (let l = 0; l < 8; l++) {
+
+            if (l == 0) {direction = (-1 - fraction) * checkBack;} 
+            else if (l == 1) {direction = -1;}
+            else if (l == 2) {direction = (-1 + fraction) * checkFront;}
+            else if (l == 3) {direction = (-fraction) * checkBack;}
+            else if (l == 4) {direction =  fraction * checkFront;}
+            else if (l == 5) {direction = (1 - fraction) * checkBack;}
+            else if (l == 6) {direction = 1;}
+            else if (l == 7) {direction = (1 + fraction) * checkFront;}
+
+            checkPos = (clickedXY + direction).toFixed(4)
+
+            
 
             if (checkPos == bombXY) {
                 bombsNear++;
-                console.log('found')
+                console.log('found', checkPos, bombXY)
             }
         }
     }
@@ -84,15 +99,20 @@ function checkHit() {
     for (let i = 0; i < bombLocations.length; i++) {
         if (clicked == bombLocations[i]) {
             console.log('BOOM');
+            bombsNear = "";
             clickedBtn.classList.add('boom')
         } else if (clicked != bombLocations[i]) {
             clickedBtn.classList.add('safe')
             if (bombsNear == 0) {
-                bombsNear = ""
+                bombsNear = "";
             }
             clickedBtn.innerHTML = bombsNear;
         }
     }
+}
+
+function placeFlag() {
+
 }
 
 function reset() {
