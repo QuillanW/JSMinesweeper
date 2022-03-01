a262cce24f0fca8bf80494f06c345ac2551c1353
QuillanW <quillanwielhouwer@live.nl>
2022-03-01 18:08:47 +0100

    update

---
+++ snake/script.js
@@ -0,0 +1,79 @@
+let direction = '';
+let pressedKey = '';
+let headLocation = 10.50
+
+function createGame() {
+    document.getElementById('startPage').style.display = 'none'
+    let startGameInterval = 1000 - ((document.getElementById('startInterval').value - 1) * 100)
+    if (startGameInterval < 100) {
+        window.alert('Invalid game speed')
+        location.reload()
+    }
+    setInterval(gameTick, startGameInterval)
+    console.log(startGameInterval)
+    document.addEventListener('keydown', function(e) {
+        pressedKey = e.key
+    });
+
+    var container = document.getElementById('container')
+    container.style.display = 'grid';
+
+    for (let i = 1; i <= 361; i++) {
+        var newSpot = document.createElement('div');
+        newSpot.id = (i / 20 - 0.05).toFixed(2);
+        newSpot.classList.add('spot');
+        container.appendChild(newSpot);
+
+        var posX = 0.05;
+        var posY = 0.05;
+
+        if (posX > 20) {
+            posY++;
+            posX = 0.05;
+        } else {
+            posX++;
+        }
+
+        newSpot.style.grid = posX + '/' + posY;
+    }
+    document.getElementById(headLocation.toFixed(2)).classList.add('head')
+}
+
+function gameTick() {
+    document.getElementById(headLocation.toFixed(2)).classList.remove('head')
+    if (pressedKey == 'a' && direction == 'd') {
+        pressedKey = 'd';
+    } else if (pressedKey == 'd' && direction == 'a') {
+        pressedKey = 'a';
+    } else if (pressedKey == 'w' && direction == 's') {
+        pressedKey = 's';
+    } else if (pressedKey == 's' && direction == 'w') {
+        pressedKey = 'w';
+    }
+
+    var oldHeadlocation = headLocation
+    
+    if (pressedKey == 'w' && direction != 's') {
+        headLocation--;
+    } else if (pressedKey == 'a' && direction != 'd') {
+        headLocation = headLocation - 0.05;
+    } else if (pressedKey == 's' && direction != 'w') {
+        headLocation++;
+    } else if (pressedKey == 'd' && direction != 'a') {
+        headLocation = headLocation + 0.05;
+    }
+
+    if (headLocation >= 20 || headLocation < 0) {
+        gameEnd(false)
+    }
+
+    direction = pressedKey;
+    document.getElementById(headLocation.toFixed(2)).classList.add('head')
+}
+
+function gameEnd(win) {
+    if (win == false) {
+        window.alert('You lost!')
+        location.reload()
+    }
+}
