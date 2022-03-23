ea97158522aee13a7b0dc7f5799ba093d9fec652
QuillanW <56843492+QuillanW@users.noreply.github.com>
2022-03-23 14:29:53 +0100

    preset added

---
+++ goalie/script.js
@@ -0,0 +1,5 @@
+function createGame() {
+    document.getElementById('startPage').style.display = 'none'
+    document.getElementById('startPage').style.display = ''
+    document.getElementById('startPage').style.display = ''
+}
