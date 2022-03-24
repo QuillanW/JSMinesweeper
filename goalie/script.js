cf7e4b5a365880fe8f3dff6ac44eb3efa09ac4a9
QuillanW <quillanwielhouwer@live.nl>
2022-03-24 12:13:29 +0100

    update

--- goalie/script.js
+++ goalie/script.js
@@ -1,5 +1,16 @@
+const gameContainer = document.getElementById('container')
+
 function createGame() {
     document.getElementById('startPage').style.display = 'none'
-    document.getElementById('startPage').style.display = ''
-    document.getElementById('startPage').style.display = ''
+    document.getElementById('gameControls').style.display = 'block'
+    gameContainer.style.display = 'block'
+
+    var newGoal = document.createElement('img')
+    newGoal.id = 'goal'
+    newGoal.src = 'goal.png'
+    gameContainer.appendChild(newGoal)
+
+    var newPlayer = document.createElement('img')
+    newPlayer.id = 'player'
+    newPlayer.src = ''
 }
