8f6755151e704dba21d969e65cc4b8392910fe29
QuillanW <56843492+QuillanW@users.noreply.github.com>
2022-03-28 13:09:25 +0200

    Update script.js

--- goalie/script.js
+++ goalie/script.js
@@ -2,4 +2,6 @@ function createGame() {
     document.getElementById('startPage').style.display = 'none'
     document.getElementById('startPage').style.display = ''
     document.getElementById('startPage').style.display = ''
+
+    
 }
