f0ececc6cdbff2955b25655840787c0470d35574
QuillanW <56843492+QuillanW@users.noreply.github.com>
2022-05-31 14:57:57 +0200

    update

---
+++ HangMan/script.js
@@ -0,0 +1,5 @@
+function createGame() {
+    document.getElementById('startPage').style.display = 'none'
+    document.getElementById('gameControls').style.display = ''
+    document.getElementById('container').style.display = ''
+}
