be87a2062d8e5551eeead31fa461b2e2310ecb22
QuillanW <56843492+QuillanW@users.noreply.github.com>
2022-05-31 14:49:43 +0200

    update

--- preset/script.js
+++ preset/script.js
@@ -1,5 +1,5 @@
 function createGame() {
     document.getElementById('startPage').style.display = 'none'
-    document.getElementById('startPage').style.display = ''
-    document.getElementById('startPage').style.display = ''
+    document.getElementById('gameControls').style.display = ''
+    document.getElementById('container').style.display = ''
 }
